9e68f1ae03fb852012c14342a225341793f59ff0
Akanbi Omolade <omoladegraham@gmail.com>
2025-09-30 17:37:01 +0100

    framer motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <div
       className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
-      style={{ backgroundImage: "url('/header_img.jpg')" }}
+      style={{ backgroundImage: "url('/header_img.png')" }}
       id="Header"
     >
       <Navbar />
-      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white"
+      >
         <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20">
           Find Your Dream Home Today
         </h2>
@@ -25,7 +32,7 @@ const Header = () => {
             Contact Us
           </a>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
